7ba828a7125f1a1618cf26ed5ec5e4fd21f836b5
Victor-Julian-Cardona <victor.cardona5@gmail.com>
2023-12-18 13:55:50 -0400

    Spotify API working

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,74 @@
-import { useState } from 'react'
-import './App.css'
-import './assets/getToken'
+import { useEffect, useState } from 'react';
+
+import axios from 'axios';
+
+import { Buffer } from 'buffer';
+
+import ReactAudioPlayer from 'react-audio-player';
+import './App.css';
+import Navbar from './components/Navbar';
+// import './assets/getToken';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [spotifyApiToken, setSpotifyApiToken] = useState('')
+  const [searchTerm, setSearchTerm] = useState('')
+  const [searchResults, setSearchResults] = useState([])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    axios.get(`https://api.spotify.com/v1/search?q=${searchTerm}&type=track`, {
+      headers: { Authorization: `Bearer ${spotifyApiToken}` }
+    })
+    .then((results) => {
+      console.log("Search results", results.data)
+      setSearchResults(results.data.tracks.items)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  }
+  
+  useEffect(() => {
+    axios.post('https://accounts.spotify.com/api/token', { grant_type: 'client_credentials' }, {
+      headers: {
+        'content-type': 'application/x-www-form-urlencoded',
+        'Authorization': 'Basic ' + (Buffer.from(import.meta.env.VITE_SPOTIFY_ID + ':' + import.meta.env.VITE_SPOTIFY_SECRET).toString('base64'))
+      }
+    })
+      .then((response) => {
+        console.log("Spotify response", response)
+        setSpotifyApiToken(response.data.access_token)
+      })
+      .catch((err) => {
+        console.log("Spotify/Axios Error ==>", err)
+      })
+  }, [])
 
   return (
     <>
+      <Navbar />
       <div>
         <p>balls</p>
-        <spotifyEmbed />
+        {/* <spotifyEmbed /> */}
+      </div>
+
+      <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Search for Track
+            <input type='text' value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
+        </label>
+        <button type='submit'>Search</button>
+      </form>
+
+      {
+        searchResults.map((result, i) => {
+          return <div key={result.id}>
+            <ReactAudioPlayer src={result.preview_url} controls />
+            <h2>{i}</h2>
+          </div>
+        })
+      }
       </div>
     </>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
-import Navbar from './components/Navbar';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App';
 import './index.css';
-import About from './pages/About';
-import Home from './pages/Home';
-import Next from './pages/Next';
-import Previous from './pages/Previous';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
-    <Navbar />
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/next" component={Next} />
-        <Route path="/previous" component={Previous} />
-    </Switch>
+      <App />
     </Router>
   </React.StrictMode>,
 )
